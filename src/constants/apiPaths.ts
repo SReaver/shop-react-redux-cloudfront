1cc5304cf20007dcae9b1414f29b01e674812914
Sergey Plotnikov <reaverz@yandex.ru>
2023-04-02 13:16:00 +0600

    add api

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,9 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product:
+    "https://kciz2cmqole7z3ahj4lvct2oba0uoxwf.lambda-url.eu-west-1.on.aws",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://kciz2cmqole7z3ahj4lvct2oba0uoxwf.lambda-url.eu-west-1.on.aws",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
