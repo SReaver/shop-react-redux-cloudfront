8b9ca0431a03bd7326f0754c46e406be445821b8
Sergey Plotnikov <reaverz@yandex.ru>
2023-04-06 11:18:21 +0600

    Change api url

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,8 @@
 const API_PATHS = {
-  product:
-    "https://kciz2cmqole7z3ahj4lvct2oba0uoxwf.lambda-url.eu-west-1.on.aws",
+  product: "https://8ewj224z02.execute-api.eu-west-1.amazonaws.com/dev",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://kciz2cmqole7z3ahj4lvct2oba0uoxwf.lambda-url.eu-west-1.on.aws",
+  bff: "https://8ewj224z02.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
